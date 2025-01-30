4b0553567b947c44c85b8d0609bc5211783e2251
bariscanatakli <bariscanatakli@users.noreply.github.com>
2025-01-30 13:25:39 +0300

    deploy fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     // Add other Next.js config options here
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/peaZ/' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/peaZ-OS/' : '',
     basePath: process.env.NODE_ENV === 'production' ? '/peaZ-OS' : '',
     output: 'export',
 };
